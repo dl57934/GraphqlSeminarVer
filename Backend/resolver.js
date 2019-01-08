006b24bd4e35409632dab22e7fa46c39afa03761
dl57934 <dl57934@pukyong.ac.kr>
2019-01-09 00:54:18 +0900

    set Basic Example

--- Backend/resolver.js
+++ Backend/resolver.js
@@ -1,7 +1,20 @@
 const resolver = {
   Query: {
-    firstSchema: () => "hi Graphql"
+    firstSchema: () => "hi Graphql",
+    schemaWithArguments: (_, { id, age }) => `${id} 나이: ${age}`,
+    getMovieDetail: (_, { id, name, story }) => getMovie(id, name, story)
+  },
+  Mutation: {
+    changeAge: (_, { id }) => id
   }
+};
+
+const getMovie = (id, name, story) => {
+  return {
+    actor: id,
+    name,
+    story: "뉴트와 동물 친구들이 함께 떠나는 환상의 모험"
+  };
 };
 
 export default resolver;
