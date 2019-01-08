3c0ae6c4127028eb41d6789c1516b0201cb0f235
dl57934 <dl57934@pukyong.ac.kr>
2019-01-08 23:47:20 +0900

    reivew 1

--- Backend/index.js
+++
@@ -1,9 +0,0 @@
-import { GraphQLServer } from "graphql-yoga";
-import Resolvers from "./javascript/Resolver";
-
-const server = new GraphQLServer({
-  typeDefs: "./graphql/schema.graphql",
-  resolvers: Resolvers
-});
-
-server.start(() => console.log("graphql Start"));
